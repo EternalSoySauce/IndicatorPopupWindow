f05551b9d169874c60636f65fab6e9ce03298aa6
yuhongfeng <1018717197@qq.com>
2021-10-09 16:48:49 +0800

    格式化

--- lib/src/main/java/com/ess/indicator/popupwindow/IndicatorPopupWindow.kt
+++ lib/src/main/java/com/ess/indicator/popupwindow/IndicatorPopupWindow.kt
@@ -45,14 +45,7 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
      * @param offsetY 默认竖直方向箭头和AnchorView边距为0（紧挨着），offsetY为在此基础上的偏移量，relativeY为TOP时向上偏移，为BOTTOM时向下偏移
      * @param ignoreStatusBar 自适应计算弹窗高度时是否忽略statusBar高度（默认为false），建议全屏时设为true，否则为false
      */
-    fun showAtAnchorView(
-        view: View,
-        relativeX: RelativeX = RelativeX.RIGHT,
-        relativeY: RelativeY = RelativeY.BOTTOM,
-        offsetX: Int = 0,
-        offsetY: Int = 0,
-        ignoreStatusBar: Boolean = false
-    ) {
+    fun showAtAnchorView(view: View, relativeX: RelativeX = RelativeX.RIGHT, relativeY: RelativeY = RelativeY.BOTTOM, offsetX: Int = 0, offsetY: Int = 0, ignoreStatusBar: Boolean = false) {
         val context = view.context
         val rootLayout = LinearLayout(context)
         val contentLayout = createContentLayout(context)
@@ -87,18 +80,13 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
         when (relativeX) {
             RelativeX.LEFT -> {
                 while (true) {
-                    arrowLayoutParams.leftMargin =
-                        (contentLayout.measuredWidth - indicatorParams.arrowWidth - arrowMargin)
+                    arrowLayoutParams.leftMargin = (contentLayout.measuredWidth - indicatorParams.arrowWidth - arrowMargin)
                             .coerceIn(0, contentLayout.measuredWidth - indicatorParams.arrowWidth)
-                    realX =
-                        viewLocation[0] + view.width / 2 - arrowLayoutParams.leftMargin - indicatorParams.arrowWidth / 2 - offsetX
+                    realX = viewLocation[0] + view.width / 2 - arrowLayoutParams.leftMargin - indicatorParams.arrowWidth / 2 - offsetX
                     if (contentLayout.layoutParams.width == 0) {
                         break
                     } else if (realX - indicatorParams.marginScreenHor < 0) {
-                        contentLayout.layoutParams.width =
-                            (contentLayout.measuredWidth - (indicatorParams.marginScreenHor - realX)).coerceAtLeast(
-                                0
-                            )
+                        contentLayout.layoutParams.width = (contentLayout.measuredWidth - (indicatorParams.marginScreenHor - realX)).coerceAtLeast(0)
                         rootLayout.measure(0, 0)
                     } else {
                         break
@@ -107,24 +95,16 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
             }
             RelativeX.MIDDLE -> {
                 while (true) {
-                    arrowLayoutParams.leftMargin =
-                        ((contentLayout.measuredWidth - indicatorParams.arrowWidth) / 2 + arrowMargin)
+                    arrowLayoutParams.leftMargin = ((contentLayout.measuredWidth - indicatorParams.arrowWidth) / 2 + arrowMargin)
                             .coerceIn(0, contentLayout.measuredWidth - indicatorParams.arrowWidth)
-                    realX =
-                        viewLocation[0] + view.width / 2 - arrowLayoutParams.leftMargin - indicatorParams.arrowWidth / 2 + offsetX
+                    realX = viewLocation[0] + view.width / 2 - arrowLayoutParams.leftMargin - indicatorParams.arrowWidth / 2 + offsetX
                     if (contentLayout.layoutParams.width == 0) {
                         break
                     } else if (realX - indicatorParams.marginScreenHor < 0) {
-                        contentLayout.layoutParams.width =
-                            (contentLayout.measuredWidth - (indicatorParams.marginScreenHor - realX)).coerceAtLeast(
-                                0
-                            )
+                        contentLayout.layoutParams.width = (contentLayout.measuredWidth - (indicatorParams.marginScreenHor - realX)).coerceAtLeast(0)
                         rootLayout.measure(0, 0)
                     } else if (realX + contentLayout.measuredWidth + indicatorParams.marginScreenHor > screenSize[0]) {
-                        contentLayout.layoutParams.width =
-                            (contentLayout.measuredWidth - (realX + contentLayout.measuredWidth + indicatorParams.marginScreenHor - screenSize[0])).coerceAtLeast(
-                                0
-                            )
+                        contentLayout.layoutParams.width = (contentLayout.measuredWidth - (realX + contentLayout.measuredWidth + indicatorParams.marginScreenHor - screenSize[0])).coerceAtLeast(0)
                         rootLayout.measure(0, 0)
                     } else {
                         break
@@ -132,17 +112,10 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
                 }
             }
             else -> {
-                arrowLayoutParams.leftMargin = arrowMargin.coerceIn(
-                    0,
-                    contentLayout.measuredWidth - indicatorParams.arrowWidth
-                )
-                realX =
-                    viewLocation[0] + view.width / 2 - arrowLayoutParams.leftMargin - indicatorParams.arrowWidth / 2 + offsetX
+                arrowLayoutParams.leftMargin = arrowMargin.coerceIn(0, contentLayout.measuredWidth - indicatorParams.arrowWidth)
+                realX = viewLocation[0] + view.width / 2 - arrowLayoutParams.leftMargin - indicatorParams.arrowWidth / 2 + offsetX
                 if (realX + contentLayout.measuredWidth + indicatorParams.marginScreenHor > screenSize[0]) {
-                    contentLayout.layoutParams.width =
-                        (contentLayout.measuredWidth - (realX + contentLayout.measuredWidth + indicatorParams.marginScreenHor - screenSize[0])).coerceAtLeast(
-                            0
-                        )
+                    contentLayout.layoutParams.width = (contentLayout.measuredWidth - (realX + contentLayout.measuredWidth + indicatorParams.marginScreenHor - screenSize[0])).coerceAtLeast(0)
                     rootLayout.measure(0, 0)
                 }
             }
@@ -151,15 +124,11 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
         when (relativeY) {
             RelativeY.TOP -> {
                 while (true) {
-                    realY =
-                        viewLocation[1] - contentLayout.measuredHeight - indicatorParams.arrowHeight - offsetY
+                    realY = viewLocation[1] - contentLayout.measuredHeight - indicatorParams.arrowHeight - offsetY
                     if (contentLayout.layoutParams.height == 0) {
                         break
                     } else if (realY - statusBarHeight - indicatorParams.marginScreenVer < 0) {
-                        contentLayout.layoutParams.height =
-                            (contentLayout.measuredHeight - (indicatorParams.marginScreenVer + statusBarHeight - realY)).coerceAtLeast(
-                                0
-                            )
+                        contentLayout.layoutParams.height = (contentLayout.measuredHeight - (indicatorParams.marginScreenVer + statusBarHeight - realY)).coerceAtLeast(0)
                         rootLayout.measure(0, 0)
                     } else {
                         break
@@ -168,11 +137,9 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
             }
             else -> {
                 realY = viewLocation[1] + view.height + offsetY
-                val bottomPos =
-                    realY + contentLayout.measuredHeight + indicatorParams.arrowHeight - statusBarHeight + indicatorParams.marginScreenVer
+                val bottomPos = realY + contentLayout.measuredHeight + indicatorParams.arrowHeight - statusBarHeight + indicatorParams.marginScreenVer
                 if (bottomPos > screenSize[1]) {
-                    contentLayout.layoutParams.height =
-                        (contentLayout.measuredHeight - (bottomPos - screenSize[1])).coerceAtLeast(0)
+                    contentLayout.layoutParams.height = (contentLayout.measuredHeight - (bottomPos - screenSize[1])).coerceAtLeast(0)
                 }
             }
         }
@@ -181,10 +148,7 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
         showAtLocation(view, Gravity.TOP or Gravity.START, realX, realY)
     }
 
-    private fun createArrowView(
-        context: Context,
-        @TriangleDrawable.ArrowDirection arrowDirection: Int
-    ): View {
+    private fun createArrowView(context: Context, @TriangleDrawable.ArrowDirection arrowDirection: Int): View {
         val view = View(context)
         view.background = TriangleDrawable(arrowDirection, indicatorParams.bgColor)
         return view
@@ -196,16 +160,12 @@ class IndicatorPopupWindow(context: Context, private val indicatorParams: Indica
         drawable.cornerRadius = indicatorParams.bgRadius.toFloat()
 
         val contentLayout = FrameLayout(context)
-        contentLayout.layoutParams = FrameLayout.LayoutParams(
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
+        contentLayout.layoutParams = FrameLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         contentLayout.background = drawable
 
         val textView: TextView
         if (indicatorParams.contentLayoutResId != 0 && indicatorParams.contentTextViewId != 0) {
-            val contentView = LayoutInflater.from(context)
-                .inflate(indicatorParams.contentLayoutResId, null, false)
+            val contentView = LayoutInflater.from(context).inflate(indicatorParams.contentLayoutResId, null, false)
             textView = contentView.findViewById(indicatorParams.contentTextViewId)
             contentLayout.addView(contentView)
         } else {
